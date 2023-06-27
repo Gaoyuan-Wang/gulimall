ee5c02ae8b484fd9db5e001c3a4d9722672dd776
Gaoyuan-Wang <wanggaouyuan@icloud.com>
2023-06-27 12:05:32 +0800

    逆向工程创建

--- gulimall-product/src/main/java/top/gaoyuanwang/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/top/gaoyuanwang/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -36,14 +36,24 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         List<CategoryEntity> entities = baseMapper.selectList(null);
         //组装成父子的树形结构
         //1.找到所有的一级分类
-        List<CategoryEntity> level1Menus = entities.stream().filter((categoryEntity) ->
-                categoryEntity.getParentCid() == 0).map((menu) ->{
+        List<CategoryEntity> level1Menus = entities.stream().filter((categoryEntity) ->{
+                log.warn(categoryEntity.toString());
+                return categoryEntity.getParentCid() == 0;})
+                .map((menu) ->{
             menu.setChildren(getChildren(menu,entities));
             return menu;
         }).sorted((menu1, menu2) ->{
             return (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort());
         }).collect((Collectors.toList()));
         return level1Menus;
+    }
+
+    @Override
+    public void removeMenuByIds(List<Long> aslist) {
+        //TODO 检查当前删除的菜单，是否被别的地方引用
+
+        //逻辑删除
+        baseMapper.deleteBatchIds(aslist);
     }
 
     private  List<CategoryEntity> getChildren(CategoryEntity root,List<CategoryEntity> all){
